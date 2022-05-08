0e55e39d5748de0bda371b661c1a7e0cbfd68c5e
LAPTOP-6DELA406\Simas <blizard1986@gmail.com>
2022-05-08 22:33:06 +0300

    bandymas

---
+++ Drivers/Browsers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knygos.lt.Drivers
+{
+    enum Browsers
+    {
+        Chrome,
+        IncognitoChrome,
+        Firefox,
+        Edge,
+        Safari,
+        InternetExplorer,
+        Opera
+    }
+}
---
+++ Drivers/CustomDriver.cs
@@ -0,0 +1,67 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knygos.lt.Drivers
+{
+    
+        class CustomDriver
+        {
+            public static IWebDriver GetChromeDriver()
+            {
+                return GetDriver(Browsers.Chrome);
+            }
+
+            public static IWebDriver GetFirefoxDriver()
+            {
+                return GetDriver(Browsers.Firefox);
+            }
+
+            public static IWebDriver GetIncognitoChrome()
+            {
+                return GetDriver(Browsers.IncognitoChrome);
+            }
+
+            private static IWebDriver GetDriver(Browsers browser)
+            {
+                IWebDriver webDriver = null;
+
+                switch (browser)
+                {
+                    case Browsers.Chrome:
+                        webDriver = new ChromeDriver();
+                        break;
+                    case Browsers.Firefox:
+                        webDriver = new FirefoxDriver();
+                        break;
+                    case Browsers.IncognitoChrome:
+                        webDriver = GetChromeWithIncognitoOption();
+                        break;
+                    default:
+                        webDriver = new ChromeDriver();
+                        break;
+                }
+
+                webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                webDriver.Manage().Window.Maximize();
+
+                return webDriver;
+            }
+
+            private static IWebDriver GetChromeWithIncognitoOption()
+            {
+                ChromeOptions options = new ChromeOptions();
+
+                // options.AddArgument("incognito");
+                options.AddArguments("headless", "incognito");
+
+                return new ChromeDriver(options);
+            }
+
+        }
+}
